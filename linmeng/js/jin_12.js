a508808316817009607225b0f55799e74f11d027
LinMeng <xiaomeng_lin@qq.com>
2023-01-03 14:50:41 +0800

    Site updated: 2023-01-03 14:50:39

---
+++ linmeng/js/jin_12.js
@@ -0,0 +1,12 @@
+function displayWindow(){
+
+    document.getElementById("window").style.display = "block";
+
+    document.getElementById("shadow").style.display = "block";
+}
+
+
+function hideWindow(){
+    document.getElementById("window").style.display = "none";
+    document.getElementById("shadow").style.display = "none";
+}
